16afe3f57aa1cc8a62118a19b8a2ba062e3b14b0
natan-teklie <natanmitiku232@gmail.com>
2024-05-08 11:11:25 +0300

    fixed

---
+++ tailwindd-project/src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="card ">This is a card.</div>
+  )
+}
+
+export default App
+
+
+
+
+
+// import "./App.css";
+
+// const App = () => {
+//   return (
+//     <div><section className="h-screen bg-twitter-blue flex items-center justify-center m-12 mx-1 text-center text-3xl p-0 md:p-14 lg:p-16 xl:p-20"><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis iste assumenda suscipit sed sint incidunt? In, eos nihil! At delectus qui voluptatum culpa in nisi totam recusandae suscipit libero ducimus?</p></section></div>
+//   )
+// }
+
+// export default App
